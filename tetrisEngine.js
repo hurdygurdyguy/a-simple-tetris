10705ae9047d69816aa222c927dffa31091bb13d
hurdygurdyguy <sebastian90ruiz33@gmail.com>
2025-12-09 14:42:35 -0300

    first commit

---
+++ tetrisEngine.js
@@ -0,0 +1,48 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    const width = 10
+    const grid = document.querySelector('.grid')
+    const ScoreDisplay = document.querySelector('#score')
+    const StartBtn = document.querySelector('start-btn')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    
+    const lTetromino = [
+        [1, width+1,width*2 + 1, 2], //Primera rotacion de "L"
+        [width, width + 1,width + 2, width*2 + 2],
+        [1, width + 1,width*2 + 1, width*2],
+        [width, width*2 ,width*2 + 1, width*2 + 2],
+    ] 
+
+    const oTetromino = [
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1],
+        [0,1,width,width+1]
+    ]  
+
+    const theTetrominoes = [lTetromino, oTetromino]
+
+    let currentPosition = 4
+    let currentRotation = 0 // esto debe cambiarlo el jugador con algun boton
+
+    let random = Math.floor(Math.random()*theTetrominoes.length)
+    let current = theTetrominoes[random][currentRotation] //Primera rotacion de tetromino "L" dentro del array
+
+    //Draw the tetromino
+    function draw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.add("tetromino")
+        })
+    }
+
+    function undraw(){
+        current.forEach(index => {
+            squares[currentPosition + index].classList.remove("tetromino")
+        })
+    }
+
+    draw()
+
+
+    //console.log(document.querySelectorAll('.grid div'))
+})
